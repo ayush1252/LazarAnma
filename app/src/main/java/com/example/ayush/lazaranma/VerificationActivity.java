dbd432d2016330f76b6e63444d3f69787da35692
Ayush Aggarwal <lookforayush@gmail.com>
2018-03-31 02:10:11 +0530

    Push for work

--- app/src/main/java/com/example/ayush/lazaranma/VerificationActivity.java
+++ app/src/main/java/com/example/ayush/lazaranma/VerificationActivity.java
@@ -1,11 +1,19 @@
 package com.example.ayush.lazaranma;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +23,15 @@ public class VerificationActivity extends AppCompatActivity implements MyAdapter
 
         private RecyclerView recyclerView;
         MyAdapter adapter;
-        List<Information> data=new ArrayList<>();
+    final Context c = this;
+
+    List<Information> data=new ArrayList<>();
+        Button b;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_verification);
+            b=findViewById(R.id.offline);
             recyclerView= (RecyclerView) findViewById(R.id.recycler);
             int[]icons={R.drawable.folder,R.drawable.dropper,R.drawable.masculine,R.drawable.stethoscope};
             for(int i=0; i<icons.length; i++)
@@ -35,6 +47,36 @@ public class VerificationActivity extends AppCompatActivity implements MyAdapter
             GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2);
             recyclerView.setLayoutManager(gridLayoutManager);
 
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+                    View mView = layoutInflaterAndroid.inflate(R.layout.alert_dialog_fetch, null);
+                    AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                    alertDialogBuilderUserInput.setView(mView);
+                    final TextInputEditText et_adhrnumber=mView.findViewById(R.id.et_adhrnumber);
+                    final TextView message=mView.findViewById(R.id.tv_message);
+                    alertDialogBuilderUserInput
+                            .setCancelable(false)
+                            .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Toast.makeText(c, "Yoyo", Toast.LENGTH_SHORT).show();
+                                    dialogInterface.cancel();
+                                }
+                            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+                    AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                    alertDialogAndroid.show();
+                }
+
+            });
+
 
 
         }
@@ -42,9 +84,26 @@ public class VerificationActivity extends AppCompatActivity implements MyAdapter
 
     @Override
     public void itemClicked(View v, int position) {
-        Intent intent = new Intent(getApplication(),RegistrationMother.class);
-        intent.putExtra("name",position);
-        startActivity(intent);
+            if(position==0) {
+                Intent intent = new Intent(getApplication(), RegistrationMother.class);
+                intent.putExtra("name", position);
+                startActivity(intent);
+            }
+            else if(position==2)
+            {
+                Intent intent = new Intent(getApplication(), RegisterationChildrenActivity.class);
+                intent.putExtra("name", position);
+                startActivity(intent);
+            }
+            else if(position==1)
+            {
+                //Query Mother Table
+                Toast.makeText(this, "Banana Hai abhi", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                //Query Child Table
+            }
     }
 
 }
